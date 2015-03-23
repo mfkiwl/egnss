4b9922e97a44795fa7db1da806f5a0b5ae331e87
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2015-03-23 22:07:31 +1000

    Refactor

--- board/rpi/post-image.sh
+++ board/rpi/post-image.sh
@@ -15,77 +15,38 @@ ROOTDIR=$IMAGEDIR/root
 mkdir -p $BOOTDIR
 mkdir -p $ROOTDIR
 
+# Name of boot image
+BOOTIMG="$IMAGEDIR/boot.img"
+
 # 64M for the boot partition
 BOOTSIZE="67108864"
 BOOTM="64M"
 
-# Estimate the root partition size
-ROOTSIZE="`du -b $ROOTFS | cut -f 1 | awk '{print $1 * 2}'`"
+# Root image already made
+ROOTIMG="$ROOTFS"
 
-unmount_img() {
-    sync
-    if [ command -v guestunmount >/dev/null 2>&1 ]; then
-	pid="$(cat guestmount.pid)"
-	guestunmount "$1"
+echo -e ${INFO}Making $BOOTIMG of size $BOOTSIZE${NC}
+# Allocate enough for the boot part
+$HOST_DIR/usr/bin/fallocate -l $BOOTSIZE "$BOOTIMG"
 
-	count=10
-	while kill -o "$pid" 2>/dev/null && [ $count -gt 0]; do
-	    sleep 1
-	    ((count--))
-	done
-	if [ $count -eq 0 ]; then
-	    echo -e "${ERROR}timed out waiting for guestmount${NC}"
-	    return 1
-	fi
-    else
-	sudo umount "$1"
-    fi
-}
+if [ $? -eq 0 ]; then
+    echo -e ${INFO}Making vfat filesystem on $IMG${NC}
+    $HOST_DIR/usr/sbin/mkfs.vfat "$BOOTIMG"
+fi
 
-mount_img() {
-    IMG=$1
-    DIR=$2
-    ARGS=$3
-
-    if [ command -v guestmount >/dev/null 2>&1 ]; then
-	guestmount -a "$IMG" -i --pid-file guestmount.pid "$DIR"
-    else
-	# Fall back to using loop
-	echo -e ${WARN}guestmount not found. Use loop device.${NC}
-	sudo mount -o loop,owner,dev,suid,user,$ARGS  "$IMG" "$DIR"
-	sudo chmod -R a+rwx "$DIR"
-	sleep 1
-    fi
-}
-
-make_img () {
-    IMG=$1
-    SIZE=$2
-    DIR=$3
-    FS=$4
-    MKFS_ARGS=$5
-    MNT_ARGS=$6
-
-    echo -e ${INFO}Making $IMG of size $SIZE${NC}
-
-    # Make image here - use quick fallocate if possible
-    $HOST_DIR/usr/bin/fallocate -l $SIZE "$IMG"
-
-    if [ $? -eq 0 ]; then
-	echo -e ${INFO}Making $FS filesystem on $IMG${NC}
-	mkfs -t $FS $MKFS_ARGS "$IMG"
-    fi
-
-    if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]; then
 	echo -e ${INFO}Mounting $IMG to $DIR${NC}
-	mount_img "$IMG" "$DIR" "$MNT_ARGS"
+    if [ command -v guestmount >/dev/null 2>&1 ]; then
+	    guestmount -a "$BOOTIMG" -i --pid-file guestmount.pid "$BOOTDIR"
+    else
+	    # Fall back to using loop
+	    echo -e ${WARN}guestmount not found. Use loop device.${NC}
+	    sudo mount -o loop,umask=0  "$BOOTIMG" "$BOOTDIR"
+	    sudo chmod -R a+rwx "$BOOTDIR"
+	    sleep 1
     fi
-}
+fi
 
-
-# Name of boot image
-BOOTIMG="$IMAGEDIR/boot.img"
-make_img "$BOOTIMG" $BOOTSIZE "$BOOTDIR" vfat "" umask=0
 if [ $? -ne 0 ]; then
     echo -e ${ERROR}Failed to make image. Exiting.${NC}
     exit 1
@@ -98,14 +59,27 @@ cp $IMAGEDIR/rpi-firmware/start.elf $BOOTDIR
 cp $IMAGEDIR/rpi-firmware/fixup.dat $BOOTDIR
 cp $IMAGEDIR/rpi-firmware/cmdline.txt $BOOTDIR
 
-unmount_img $BOOTDIR
+sync
+if [ command -v guestunmount >/dev/null 2>&1 ]; then
+	pid="$(cat guestmount.pid)"
+	guestunmount "$BOOTDIR"
+
+	count=10
+	while kill -o "$pid" 2>/dev/null && [ $count -gt 0]; do
+	    sleep 1
+	    ((count--))
+	done
+	if [ $count -eq 0 ]; then
+	    echo -e "${ERROR}timed out waiting for guestmount${NC}"
+	    return 1
+	fi
+else
+	sudo umount "$BOOTDIR"
+fi
 if [ $? -ne 0 ]; then
     echo -e ${ERROR} Failed to unmount image. Exiting. ${NC}
     exit 1
 fi
-
-#OK, now make the root filesystem
-ROOTIMG="$ROOTFS"
 
 # Name of the image to build
 IMAGE="$IMAGEDIR/egnss_rpi_3.19.y_$( date +%Y%m%d ).img"
@@ -114,11 +88,12 @@ echo -e ${INFO}Making $IMAGE of size $IMAGESIZE${NC}
 # Make 1 MB for the MBR and partition table
 dd if=/dev/zero of=$IMAGE bs=1M count=1
 if [ $? -ne 0 ]; then
-    echo -e ${ERROR} Failed to write MBR. ${NC}
+    echo -e ${ERROR}Failed to write MBR.${NC}
     exit 1
 fi
 
-# Concatenate
+# Concatenate, first parition is boot, then root
+# Everything is aligned to 1M
 cat $BOOTIMG >> $IMAGE
 cat $ROOTIMG >> $IMAGE
 
@@ -144,6 +119,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# Print partition information
 fdisk -l "$IMAGE"
 
 # DONE
