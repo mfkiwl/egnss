2bd7895aeff8293406411a2636e3e9111e535da7
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2015-05-15 22:18:57 +1000

    Genesis ping

---
+++ package/genesis_ping/genesis_ping.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# genesis_ping
+#
+################################################################################
+
+GENESIS_PING_VERSION = master
+GENESIS_PING_LICENSE = GPLv3
+GENESIS_PING_LICENSE_FILES = LICENSE
+GENESIS_PING_INSTALL_TARGET = YES
+GENESIS_PING_SITE = $(call github,anthony-arnold,genesis,$(GENESIS_PING_VERSION))
+GENESIS_PING_CONF_OPTS += -DMAKE_GENESIS=OFF -DMAKE_GENESIS_PING=ON
+
+$(eval $(cmake-package))
